dd7736f3fbc1545c30a3546db5b1fbac59c52de1
Amanda Lilley <yoknitting24@gmail.com>
2016-03-26 12:07:35 -0400

    Final Backend work

--- exercises/summary_exercises_two.rb
+++ exercises/summary_exercises_two.rb
@@ -2,3 +2,9 @@ arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
 
 arr.delete_if { |word| word.start_with? "s" }
 arr.delete_if { |word| word.start_with? "s" , "w" }
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+a = a.map { |pairs| pairs.split }
+a = a.flatten
+p a
